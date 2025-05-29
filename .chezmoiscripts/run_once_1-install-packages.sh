82045b4d4305d251c2bff56508843efc2d329889
Olav Landsverk <olav.landsverk@gmail.com>
2025-05-29 16:34:12 +0200

    Update .chezmoiscripts/1-install-packages.sh

--- .chezmoiscripts/run_once_1-install-packages.sh
+++ .chezmoiscripts/run_once_1-install-packages.sh
@@ -7,7 +7,6 @@ sudo apt-get install build-essential procps curl file git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 brew install gcc
 
 # install packages
