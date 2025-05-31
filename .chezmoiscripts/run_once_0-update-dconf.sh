8d4a2f7eddc16351d89bfbd60b78992714761c28
Olav Landsverk <olav.landsverk@gmail.com>
2025-05-31 15:23:14 +0200

    update

--- .chezmoiscripts/run_once_0-update-dconf.sh
+++ .chezmoiscripts/run_once_0-update-dconf.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#set system fonts to Open Sans
+# set system fonts to Open Sans
 system_font="Open Sans Bold 14"
 dconf_schema="org.gnome.desktop.interface"
 gsettings set $dconf_schema font-name "$system_font"
 gsettings set $dconf_schema document-font-name "$system_font"
 
-#set terminal font
+# set terminal font
 profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
 profile=${profile:1:-1}
 dconf_schema="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/"
 gsettings set $dconf_schema use-system-font false
 gsettings set $dconf_schema font "Monospace Bold 15"
+
+# set terminal color palette
+palette=$(python3 ~/.scripts/palette.py)
+gsettings set $dconf_schema palette $palette
