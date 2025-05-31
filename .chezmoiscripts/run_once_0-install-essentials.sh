25c07eb16971f92d67995638ebcd4ddedfe75b84
Olav Landsverk <olav.landsverk@gmail.com>
2025-05-31 19:21:11 +0200

    update

--- .chezmoiscripts/run_once_0-install-essentials.sh
+++ .chezmoiscripts/run_once_0-install-essentials.sh
@@ -8,7 +8,6 @@ sudo apt-get install build-essential procps curl file git
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-brew install gcc
 
 # install packages
 export HOMEBREW_BUNDLE_FILE=~/.brew/Brewfile 
