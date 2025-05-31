6acea4f9389407a8cd480b067d8f536cb22894a5
Olav Landsverk <olav.landsverk@gmail.com>
2025-05-31 15:24:36 +0200

    Add .scripts/palette.py

---
+++ dot_scripts/palette.py
@@ -0,0 +1,16 @@
+import json
+
+black = (0, 0, 0)
+red = (255, 130, 130)
+green = (130, 255, 130)
+yellow = (255, 210, 210)
+blue = (170, 255, 170)
+magenta = (255, 170, 170)
+cyan = (210, 255, 210)
+white = (255, 255, 255)
+
+normal_colors = [black, red, green, yellow, blue, magenta, cyan, white]
+bright_colors = normal_colors
+colors = normal_colors + bright_colors
+colors_formatted = [f"rgb{c}" for c in colors]
+print(json.dumps(colors_formatted))
