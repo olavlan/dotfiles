4eb3980ec7dde483a61471a83aaae1c58ceb8410
Olav Landsverk <olav.landsverk@gmail.com>
2025-06-02 17:09:43 +0200

    Update .chezmoiscripts/1-update-dconf.sh

--- .chezmoiscripts/run_once_1-update-dconf.sh
+++ .chezmoiscripts/run_once_1-update-dconf.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# set system fonts to Open Sans
+# set the system font to Open Sans
 system_font="Open Sans Bold 14"
 dconf_schema="org.gnome.desktop.interface"
 gsettings set $dconf_schema font-name "$system_font"
 gsettings set $dconf_schema document-font-name "$system_font"
 
-# set terminal font
+# set the terminal font
 profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
 profile=${profile:1:-1}
 dconf_schema="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/"
 gsettings set $dconf_schema use-system-font false
 gsettings set $dconf_schema font "Monospace Bold 15"
 
-# set terminal color palette
+# set the terminal color palette
 palette=$(python3 ~/.scripts/palette.py)
 gsettings set $dconf_schema palette "$palette"
 gsettings set $dconf_schema foreground-color "rgb(255,240,240)"
 gsettings set $dconf_schema background-color "rgb(0,0,0)"
 gsettings set $dconf_schema use-theme-colors false
 
-# set terminal shell
+# set the default terminal shell
 gsettings set $dconf_schema use-custom-command true
 gsettings set $dconf_schema custom-command "/home/linuxbrew/.linuxbrew/bin/zsh"
