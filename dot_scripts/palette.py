80d29910a4d71fbd0630c98b35733cebd65a05f1
Olav <olav.landsverk@gmail.com>
2025-06-19 19:11:51 +0200

    Update .scripts/palette.py

--- dot_scripts/palette.py
+++ dot_scripts/palette.py
@@ -3,11 +3,11 @@ import json
 black = (0, 0, 0)
 red = (255, 130, 130)
 green = (130, 255, 130)
-yellow = (255, 210, 210)
+yellow = (255, 170, 170)
 blue = (170, 255, 170)
-magenta = (255, 170, 170)
+magenta = (255, 150, 150)
 cyan = (210, 255, 210)
-white = (255, 255, 255)
+white = (255, 210, 210)
 
 normal_colors = [black, red, green, yellow, blue, magenta, cyan, white]
 bright_colors = normal_colors
